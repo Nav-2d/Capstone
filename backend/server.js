a06d7fc0b1ca3ea52b6816590bb1d01e9d409ac4
nav-2d <navdeep.singh2088@gmail.com>
2022-03-04 14:43:30 -0800

    Connect to DB

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log('Server');
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const connectDB = require('./config/db');
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
