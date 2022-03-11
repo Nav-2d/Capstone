142bdee1c13945372e7213f70bd270cfbf880a13
Ravnit Kaur <ravnitdeo97@yahoo.com>
2022-03-10 20:39:30 -0800

    Update TimetableDashboard.jsx

--- frontend/src/pages/TimetableDashboard.jsx
+++ frontend/src/pages/TimetableDashboard.jsx
@@ -1,43 +1,49 @@
 import React, { Component } from 'react';
 
 import { AgGridColumn, AgGridReact } from 'ag-grid-react';
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-balham.css';
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-balham.css";
 
-class TimetableDashbaord extends Component {
+class TimetableDashboard extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      rowData: [
-        { subject: 'INFO', term_code: '202210', createdAt: '01-Jan-2022' },
-        { subject: 'INFO', term_code: '202220', createdAt: '01-Jan-2022' },
-        { subject: 'INFO', term_code: '202230', createdAt: '01-Jan-2022' }
+      rowData: [],
+      colDefs: [
+        { field: 'subject' },
+        { field: 'term_code' },
+        { field: 'createdAt' },
       ]
     }
   }
 
+  componentDidMount() {
+    fetch('https://____')
+      .then(result => result.json())
+      .then(rowData => this.setState({ rowData }))
+  }
+
   render() {
     return (
-      <>
-
-        <div>Timetable Dashboard</div>
+      <><div>Timetable Dashboard</div>
         <div
           className="ag-theme-balham"
           style={{ height: '200px', width: '600px' }}
         >
           <AgGridReact
             pagination={true}
-            rowData={this.state.rowData}>
-            <AgGridColumn field="subject" sortable={ true } filter={ true }></AgGridColumn>
-            <AgGridColumn field="term_code" sortable={ true } filter={ true }></AgGridColumn>
-            <AgGridColumn field="createdAt" sortable={ true } filter={ true }></AgGridColumn>
+            defaultColDef={{ sortable: true, filter: true }}
+            rowData={this.state.rowData}
+            columnDefs={this.state.colDefs}
+          >
+
+
           </AgGridReact>
         </div>
-
       </>
     );
   }
 }
 
-export default TimetableDashbaord;
+export default TimetableDashboard;
