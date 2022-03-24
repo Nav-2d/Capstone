df1fa9dc57197329e24396a987069a98bdddf6bc
Nav-2d <navdeep.singh2088@gmail.com>
2022-03-23 21:18:29 -0700

    cleanup login page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -13,22 +13,28 @@ function Header() {
     navigate('/');
   };
   return (
-    <div className='relative bg-white'>
-      <div className='max-w-7xl mx-auto px-4 sm:px-6'>
-        <div className='flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10'>
-          <div className='flex justify-start lg:w-0 lg:flex-1'>
-            <Link to='/timetabledashboard'>University Timetable</Link>
-          </div>
-          <div className='md:flex items-center justify-end md:flex-1 lg:w-0'>
-            {user ? (
-              <div className='whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900'>
-                <button onClick={onLogout}>Logout</button>
-              </div>
-            ) : (
-              <div className='ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-red hover:bg-red'>
-                <Link to='/'>Sign In</Link>
-              </div>
-            )}
+    <div className='relative bg-black'>
+      <div className='container mx-auto'>
+        <div className='mx-auto px-4 sm:px-6'>
+          <div className='flex justify-between items-center py-6 md:justify-start md:space-x-10'>
+            <div className='flex justify-start lg:w-0 lg:flex-1'>
+              <Link to='/timetabledashboard'>
+                <span className='text-white font-bold'>
+                  University Timetable
+                </span>
+              </Link>
+            </div>
+            <div className='md:flex items-center justify-end md:flex-1 lg:w-0'>
+              {user ? (
+                <div className='whitespace-nowrap text-base font-bold text-white  hover:text-gray-400'>
+                  <button onClick={onLogout}>Logout</button>
+                </div>
+              ) : (
+                <div className='ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-red hover:bg-red'>
+                  <Link to='/'>Sign In</Link>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
