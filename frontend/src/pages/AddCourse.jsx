91912594b0eb9b541777d0daad2bd4a5d9188309
nav-2d <navdeep.singh2088@gmail.com>
2022-04-04 22:40:26 -0700

    modify styling for back icon

--- frontend/src/pages/AddCourse.jsx
+++ frontend/src/pages/AddCourse.jsx
@@ -35,7 +35,7 @@ function AddCourse() {
         <div class='text-primary font-medium text-sm  text-center inline-flex items-center'>
           <svg
             xmlns='http://www.w3.org/2000/svg'
-            className='h-6 w-6'
+            className='h-4 w-4'
             fill='none'
             viewBox='0 0 24 24'
             stroke='currentColor'
@@ -47,7 +47,9 @@ function AddCourse() {
               d='M10 19l-7-7m0 0l7-7m-7 7h18'
             />
           </svg>
-          <Link to='/course-dashboard'>Back to Course Dashboard</Link>
+          <div className='pl-2'>
+            <Link to='/course-dashboard'>Back to Course Dashboard</Link>
+          </div>
         </div>
       </div>
       <div className='max-w-sm mx-auto'>
